f5d0c64a5e2b338f7807b95b0596bc2ebbb8f826
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2024-09-07 11:36:57 -0300

    Lista 05
    Iniciando a lista 5 de exercicios python

---
+++ TDE_03/Exer_03.py
@@ -0,0 +1,9 @@
+#3. Crie um script Python que leia o dia, mês e ano de nascimento de uma pessoa e
+#mostre uma mensagem com a data formatada.
+
+'''
+day = int(input("Diga a data: "))
+month = int(input("Diga a mês: "))
+year  = int(input("Diga o ano: "))
+print(f"A data e : {day}/{month}/{year}")
+'''
---
+++ TDE_03/Exer_04.py
@@ -0,0 +1,7 @@
+#4. Organize os números 2,3,4,5,10,12 para obter a saída (resposta) 18 em uma
+#única operação
+
+'''
+res= (12 -2) + 10 - 3 - 4 + 5
+print(res)
+'''
